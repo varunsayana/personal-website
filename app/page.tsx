31f05ab83a38c4fb9c79c6d2a0dd7188b077df7f
Varun Sayana <varun.sayana@gmail.com>
2026-03-10 00:27:24 -0400

    feat: Implement a new `StaggeredMenu` component with GSAP animations and integrate it into the application's homepage.

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import { StaggeredMenu } from "@/components/StaggeredMenu";
 import HeroText from "@/components/home/herotext";
 import DarkVeil from "@/components/home/DarkVeil";
 
@@ -6,6 +7,8 @@ import DarkVeil from "@/components/home/DarkVeil";
 export default function Home() {
   return (
     <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh', width: '100vw' }}>
+
+      <StaggeredMenu isFixed={true} />
 
       <div style={{ position: 'absolute', height: '100vh', width: '100vw' }}>
 
