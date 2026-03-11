f67e33470a6fb6cebf3e55ea623b147278c33715
Varun Sayana <varun.sayana@gmail.com>
2026-03-10 20:18:09 -0400

    feat: Add a new navbar component.

--- app/page.tsx
+++ app/page.tsx
@@ -1,27 +1,33 @@
-import { StaggeredMenu } from "@/components/StaggeredMenu";
-import HeroText from "@/components/home/herotext";
-import DarkVeil from "@/components/home/DarkVeil";
+import { Header } from "@/components/navbar/header-2";
+import HeroText from "@/components/home/hero/herotext";
+import DarkVeil from "@/components/home/hero/DarkVeil";
 
 
 
 export default function Home() {
   return (
-    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh', width: '100vw' }}>
-
-      <StaggeredMenu isFixed={true} />
-
-      <div style={{ position: 'absolute', height: '100vh', width: '100vw' }}>
-
-        <DarkVeil hueShift={25} noiseIntensity={0} scanlineIntensity={0} speed={1} scanlineFrequency={0} warpAmount={1} resolutionScale={1} />
-
+    <div style={{ position: 'relative', height: '100vh', width: '100vw' }}>
+      <div style={{ position: 'absolute', height: '100vh', width: '100vw', zIndex: 100 }}>
+        <Header />
       </div>
 
-      <div style={{ position: 'relative' }}>
+      <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh', width: '100vw' }}>
 
-        <HeroText />
+        <div style={{ position: 'absolute', height: '100vh', width: '100vw' }}>
+
+          <DarkVeil hueShift={25} noiseIntensity={0} scanlineIntensity={0} speed={1} scanlineFrequency={0} warpAmount={1} resolutionScale={1} />
+
+        </div>
+
+        <div style={{ position: 'relative' }}>
+
+          <HeroText />
+
+
+
+        </div>
 
       </div>
-
     </div>
 
   );
